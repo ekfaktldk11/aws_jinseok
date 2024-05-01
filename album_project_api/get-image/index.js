be9eff30110c03373e8374839110ab0ba9fd5778
jindorry <ekfaktldk11@gmail.com>
2024-05-01 17:20:49 +0900

    bug fix - s3 bucket access denied - s3:ListBucket for ListObjectsCommand

--- album_project_api/get-image/index.js
+++ album_project_api/get-image/index.js
@@ -1,4 +1,4 @@
-const { S3Client, ListObjectsCommand, GetObjectCommand } = require("@aws-sdk/client-s3");
+const { S3Client, ListObjectsCommand, GetObjectCommand } = require('@aws-sdk/client-s3');
 
 // 특정 파일 가져오기
 const getObject = async (client, Key) => {
@@ -27,12 +27,12 @@ const getImageContents = async (client) => {
 
 exports.handler = async function (event, context) {
     try {
-        console.log("## event : ", event);
+        console.log('## event : ', event);
 
         const s3Client = new S3Client();
         const contents = await getImageContents(s3Client);
 
-        console.log("## contents : ", contents);
+        console.log('## contents : ', contents);
         // let idx = 0;
         // for (const content of contents) {
         //     console.log(`## content #${idx} : `, content);
@@ -41,14 +41,13 @@ exports.handler = async function (event, context) {
         // // const imageObj = await getObject(s3Client, Key);
         // const imageUrl = await transformToByteArrayUrl(imageObj.Body);
         // console.log("## imageObj : ", imageObj);
-
     } catch (err) {
         console.error(err);
     }
 
     return {
         headers: {
-            "Access-Control-Allow-Origin": process.env.HOST_URL,
+            'Access-Control-Allow-Origin': process.env.HOST_URL,
         },
         statusCode: 200,
     };
